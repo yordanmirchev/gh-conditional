197362fbbbf9f21b9ad0ab2ba8e9f1c7b47a3df2
Yordan Mirchev <ymm@example.com>
2023-04-28 08:53:06 +0300

    Break test.

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,7 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area')).not.toBeInTheDocument();
+    // expect(screen.getByTestId('help-area')).toBeInTheDocument();
+    expect(screen.getByTestId('FFFFFFFFF')).toBeInTheDocument();
   });
 });
